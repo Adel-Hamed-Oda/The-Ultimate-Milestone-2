002f96aa1d4b9579c09f14b35ff0bf57706ed2ee
Salma Mohamed Omar <salmonberryz@gmail.com>
2025-11-25 23:47:33 +0200

    Update The Entire Milestone.sql

--- The Entire Milestone.sql
+++ The Entire Milestone.sql
@@ -1736,6 +1736,9 @@ GO
 ---------------------------------------------------------
 -- Submit_annual
 ---------------------------------------------------------
+---------------------------------------------------------
+-- Submit_annual
+---------------------------------------------------------
 CREATE PROC Submit_annual
     @employee_id INT, 
     @replacement_emp INT, 
@@ -1822,7 +1825,7 @@ BEGIN
         DECLARE @president_ID INT;
         SELECT @president_ID = emp_ID
         FROM dbo.Employee_Role
-        WHERE LOWER(role_name) = 'president'
+        WHERE LOWER(role_name) = 'President'
           AND dbo.Is_On_Leave(emp_ID, CAST(GETDATE() AS DATE), CAST(GETDATE() AS DATE)) = 0;
 
         IF @president_ID IS NULL
@@ -1841,8 +1844,8 @@ BEGIN
         VALUES (@leave_id, @employee_id, @replacement_emp);
 
         INSERT INTO Employee_Approve_Leave 
-        VALUES (@president_ID, @request_id, 'pending'),
-               (@hr_rep, @request_id, 'pending');
+        VALUES (@president_ID, @leave_id, 'pending'),
+               (@hr_rep, @leave_id, 'pending');
 
         RETURN;
     END
@@ -1883,14 +1886,14 @@ BEGIN
     VALUES (CAST(GETDATE() AS DATE), @start_date, @end_date, 'pending');
 
     DECLARE @req_id INT;
-    SELECT @request_id = SCOPE_IDENTITY();
+    SELECT @req_id = SCOPE_IDENTITY();
 
     INSERT INTO Annual_Leave
     VALUES (@req_id, @employee_id, @replacement_emp);
 
     INSERT INTO Employee_Approve_Leave 
     VALUES (@approver1, @req_id, 'pending'),
-           (@hr_rep, @request_id, 'pending');
+           (@hr_rep, @req_id, 'pending');
 
 END
 GO
@@ -1986,7 +1989,7 @@ BEGIN
     VALUES (@leave_id, @employee_ID);
 
     INSERT INTO Employee_Approve_Leave
-    VALUES (@hr_rep, @request_id, 'pending');
+    VALUES (@hr_rep, @leave_id, 'pending');
 
 END
 GO
